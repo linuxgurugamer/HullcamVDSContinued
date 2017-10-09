3911665291df94be611d68041e46e507299af135
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-10-09 14:17:09 -0400

    Updated for KSP 1.3.1

--- Src/deploy.bat
+++ Src/deploy.bat
@@ -1,32 +1,13 @@
 ﻿
-set H=R:\KSP_1.3.0_dev
+rem @echo off
+
+set H=R:\KSP_1.3.1_dev
+set GAMEDIR=HullCameraVDS
+set VERSIONFILE=HullcamVDSContinued
+
 echo %H%
 
-set d=%H%
-if exist %d% goto one
-mkdir %d%
-:one
-set d=%H%\GameData
-if exist %d% goto two
-mkdir %d%
-:two
-set d=%H%\GameData\HullCameraVDS
-if exist %d% goto three
-mkdir %d%
-:three
-set d=%H%\GameData\HullCameraVDS\Resources
-if exist %d% goto four
-mkdir %d%
-:four
-
-
-copy /y bin\Debug\HullCamera.dll ..\GameData\HullCameraVDS\Plugins
-copy  /y HullcamVDSContinued.version ..\GameData\HullCameraVDS\HullcamVDSContinued.version
-
-xcopy /Y /E ..\GameData\HullCameraVDS %H%\GameData\HullCameraVDS
-rem copy  ..\HullCameraAssets\Bundles\shaders %H%\GameData\HullCameraVDS\Resources\shaders.bundle
-
-rem copy  ..\HullCameraAssets\Bundles-windows\shaders %H%\GameData\HullCameraVDS\Resources\shaders.windows
-rem copy  ..\HullCameraAssets\Bundles-osx\shaders %H%\GameData\HullCameraVDS\Resources\shaders.osx
-rem copy  ..\HullCameraAssets\Bundles-linux\shaders %H%\GameData\HullCameraVDS\Resources\shaders.linux
-
+copy /Y "%1%2" "..\GameData\%GAMEDIR%\Plugins"
+copy /Y %VERSIONFILE%.version ..\GameData\%GAMEDIR%
+cd ..
+xcopy /y /s /I GameData\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
